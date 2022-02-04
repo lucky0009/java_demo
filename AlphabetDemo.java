c1c14902b9286530e697be49be064d568287db71
Abhishek Kumar Chaudhary <50618757+lucky0009@users.noreply.github.com>
2022-02-05 01:15:46 +0530

    upload file

---
+++ AlphabetDemo.java
@@ -0,0 +1,11 @@
+class AlphabetDemo
+{
+  public static void main(String[] args)
+  {
+	  System.out.print("Alphabet :");
+	  for(char ch = 'a';ch <='z';ch++)
+	  {
+		  System.out.print(ch+",");
+	  }
+  }
+}
